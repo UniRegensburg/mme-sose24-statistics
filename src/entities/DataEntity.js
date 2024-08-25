211189ae904d5ee8645ebb1dd7b2ae24a0e48588
Roy_Xu <ruoyu.xv@gmail.com>
2024-08-25 11:09:46 +0200

    Fix structure

--- src/entities/DataEntity.js
+++ src/entities/DataEntity.js
@@ -1,3 +1,43 @@
-export default class DataEntity {
+/**
+ * A class for representing questionnaire data.
+ */
+class DataEntity {
+
+    /**
+     * Construct empty DataEntity
+     */
+    constructor() {
+        this.type = null
+        this.userInfo = null
+        this.result = null
+    }
+
+    /**
+     * Check if the data is valid according to the type.
+     * @return {boolean}
+     */
+    dataIsValid() {
+        return true
+    }
     
 }
+
+
+/**
+ * A class that contains information about users who took
+ * the questionnaire.
+ */
+class UserInfo {
+
+}
+
+
+/**
+ * Questionnaire results.
+ */
+class Result {
+
+}
+
+
+export {DataEntity, UserInfo, Result}
