e9f3345541104f385ee45d4d274eb1e557a1177a
SebSch80890 <sebastian1.scheruebl@stud.uni-regensburg.de>
2024-09-05 16:14:58 +0200

    Build SUS-DataStructure

--- src/entities/DataEntity.js
+++ src/entities/DataEntity.js
@@ -14,10 +14,14 @@ class DataEntity {
 
     /**
      * Check if the data is valid according to the type.
+     * DO WE NEED THAT??
      * @return {boolean}
      */
     dataIsValid() {
-        return true
+        if (this.type && this.userInfo && this.result) {
+            return true;
+        }
+        return false;
     }
     
 }
@@ -28,16 +32,42 @@ class DataEntity {
  * the questionnaire.
  */
 class UserInfo {
+    constructor(userID, age, gender, education) {
+        this.userID = userID;
+        this.age = age;
+        this.gender = gender;
+        this.education = education;
+    }
 
 }
 
+class SUSResult {
+    constructor(userID) {
+        this.userID = userID;  // Reference to class UserInfo
+        this.susScores = Array(10).fill(null);  // Array to hold scores for the 10 SUS questions (1 to 5 scale)
+    }
 
-/**
- * Questionnaire results.
- */
+    /**
+     * Set a SUS score for a specific question.
+     * @param {number} questionNr - Index of the question (0-9)
+     * @param {number} score - Score for the question (1-5)
+     */
+    setSUSScore(questionNr, score) {
+        if (questionNr >= 0 && questionNr < 10 && score >= 1 && score <= 5) {
+            this.susScores[questionNr] = score;
+        } else {
+            console.error("Problem by the SUS-Structure");
+        }
+    }
+}
+
 class Result {
-
+    constructor(userID, susResult) {
+        this.userID = userID;
+        this.susResult = susResult; 
+    }
 }
+    
 
 
-export {DataEntity, UserInfo, Result}
+export {DataEntity, UserInfo, SUSResult, Result};
